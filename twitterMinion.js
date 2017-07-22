144cec39a525c3f207e66bb826a39753aaf7604a
Austin Lovell <austin.d.lovell@gmail.com>
2017-07-22 03:48:48 -0600

    added in todo notes

--- twitterMinion.js
+++ twitterMinion.js
@@ -1,3 +1,16 @@
+/***** Things To Do ******
+ * Check if they have a BIO
+ * Scan BIO for key words
+ * Check for profile picture
+ * Check for banner picture
+ * Give score dependings on the above criteria
+ * 
+ * Follow Session History
+ * * Store followed people from session to local storage
+ * * A report task can be run to see how many people have followed back from the previous follow spree
+ * * Session can store Person page who was scraped. Terms used for Bio Search. Count of people to Follow. 
+***********************/
+
 var people = [];
 var peopleToAdd = [];
 var peopleAdded = 0;
