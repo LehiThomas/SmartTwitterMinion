bf67942167c5fff7885332bc2cde0063d7017759
Austin Lovell <adl57@ldschurch.org>
2017-07-27 12:09:47 -0600

    basic recusive loop with a promise

--- src/gatherPeopleToFollow.js
+++ src/gatherPeopleToFollow.js
@@ -2,36 +2,31 @@ import createCard  from './createCard';
 import scoreCard  from './scoreCard';
 
 function gatherPeopleToFollow(maxFollow) {
-	alert("Hello ;) ");
-	const maxFollow = maxFollow;
-	let peopleToFollow = [];
-	let previousLength = 0;
-	let people = [];
+	// Get if followers or following
+	// Get maxPeople
+	let maxPeople = 500;
 
-	while (peopleToFollow.length < maxFollow || (window.innerHeight + window.scrollY) < document.body.scrollHeight) {
-		window.scrollTo(0,document.body.scrollHeight);
-		people = document.getElementsByClassName("ProfileCard");
-		previousLength = people.length
-
-			for (var i = previousLength; i < people.length; i++) {
-			let card = createCard(this.people[i]);
-
-			if (card.checkForFollowed) {
-				continue;
-			} else {
-				let score = scoreCard;
-				if (score >= 30) {
-					peopleToFollow.push(card);
-				}
-			}
-		}
-	}
-
-	if (peopleToFollow.length !== 0) {
-		resolve(peopleToFollow);
-	} else {
-		reject('No one was added to the array');
-	}
+	return new Promise((resolve, reject) => {
+		loopGather(maxFollow, maxPeople, resolve, reject);
+	});
 }
+
+function loopGather(maxFollow, maxPeople, resolve, reject, index = 0, cards = []){
+	window.scrollTo(0, document.body.scrollHeight);
+
+	setTimeout(() => {
+		let scrolledPeople = document.getElementsByClassName("ProfileCard");
+
+		if(scrolledPeople.length >= maxPeople) return peopleToFollow;
+
+		for(; index < scrolledPeople.length && cards.length < maxFollow; index++){
+			let card = createCard(scrolledPeople[index]);
+			// Score the card here
+			cards.push(card);
+		}
+		cards.length >= maxFollow ? resolve(cards) : loopGather(maxFollow, maxPeople, resolve, reject, index, cards);
+	}, 2000);
+}
+
 
 export default gatherPeopleToFollow;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import gatherPeopleToFollow from './gatherPeopleToFollow';
 import followPeople from './followPeople';
 
 window.TwitterMinion =  (function(){
-	alert("Begin");
 	let myFollowing = 4997;
 	let myFollowers = 6238;
-	let maxFollow = 10 //assessMaxFollow(myFollowing, myFollowers);
+	let maxFollow = 100 //assessMaxFollow(myFollowing, myFollowers);
 	return {
 		follow: () => {
 			gatherPeopleToFollow(maxFollow)
-				.then(followPeople(peopleToFollow))
+				.then(cards => {
+					// Placing on window for debugging purposes
+					window.cards = cards;
+				})	
+			// .then(peopleToFollow => followPeople(peopleToFollow))
 		}
 	}
 })();
