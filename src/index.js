d0ae84d6c189079c709d2099f75cf853290c3b22
Austin Lovell <adl57@ldschurch.org>
2017-08-01 09:53:18 -0600

    adding in prompts

--- src/index.js
+++ src/index.js
@@ -3,17 +3,43 @@ import gatherCards from './gatherCards';
 import followPeople from './followPeople';
 
 window.TwitterMinion =  (function(){
-	let myFollowing = 4997;
-	let myFollowers = 6238;
+	let myFollowing = 0;
+	let myFollowers = 0;
 	let maxFollow = assessMaxFollow(myFollowing, myFollowers);
+	let passingScore = 0;
+	let bannerScore = 0;
+	let profileScore = 0;
+	let keyWords = [];
 	return {
 		follow: () => {
+			// Get Max Follow Count
+
+			// Inform on count - user can change amount if desired
+
+			// Allow User to add key words
+
 			gatherCards(maxFollow)
 				.then(cards => {
 					// Placing on window for debugging purposes
 					window.cards = cards;
 				})
 			// .then(peopleToFollow => followPeople(peopleToFollow))
+		},
+		setup: () => {
+			// Self Details
+			myFollowing = prompt("How many followers do you have?", myFollowing);
+			myFollowers = prompt("How many people are you following?", myFollowers);
+			// Scoreing Details
+			passingScore = prompt("Minimum Passing Score", passingScore);
+			bannerScore = prompt("Banner Score", bannerScore);
+			profileScore = prompt("Profile Score", profileScore);
+		},
+		test: () => {
+			console.error(`myFollowing ${myFollowing}`);
+			console.error(`myFollowers ${myFollowers}`);
+			console.error(`passingScore ${passingScore}`);
+			console.error(`bannerScore ${bannerScore}`);
+			console.error(`profileScore ${profileScore}`);
 		}
 	}
 })();
