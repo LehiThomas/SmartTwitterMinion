043faf3464da1c6dbfdeb223791c261516314db8
Austin Lovell <adl57@ldschurch.org>
2017-08-01 13:18:49 -0600

    added in config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+let config = {
+	myFollowing: 0,
+	myFollowers: 0,
+	passingScore: 0,
+	bannerScore: 0,
+	profileScore: 0,
+	keyWords:[]
+}
+
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
+import config from './config';
 import assessMaxFollow from './assessMaxFollow';
 import gatherCards from './gatherCards';
 import doClicker from './doClicker';
 import followPeople from './followPeople';
+import test from './test';
 
 window.TwitterMinion =  (function(){
-	let myFollowing = 0;
-	let myFollowers = 0;
-	let passingScore = 0;
-	let bannerScore = 0;
-	let profileScore = 0;
-	let keyWords = [];
 	let _this = {
 		follow: () => {
-			let maxFollow = assessMaxFollow(myFollowing, myFollowers);
+			let maxFollow = assessMaxFollow(config.myFollowing, config.myFollowers);
 			maxFollow = prompt(`The max that can be followed is ${maxFollow}. You may set a different amount`, maxFollow);
 
 			gatherCards(maxFollow)
@@ -28,28 +24,22 @@ window.TwitterMinion =  (function(){
 				_this.setup.promptProfileScore();
 			},
 			promptMyFollowing: () => {
-				myFollowing = prompt("How many followers do you have?", myFollowing);
+				config.myFollowing = prompt("How many followers do you have?", config.myFollowing);
 			},
 			promptMyFollowers: () => {
-				myFollowers = prompt("How many people are you following?", myFollowers);
+				config.myFollowers = prompt("How many people are you following?", config.myFollowers);
 			},
 			promptPassingScore: () => {
-				passingScore = prompt("Minimum Passing Score", passingScore);
+				config.passingScore = prompt("Minimum Passing Score", config.passingScore);
 			},
 			promptBannerScore: () => {
-				bannerScore = prompt("Banner Score", bannerScore);
+				config.bannerScore = prompt("Banner Score", config.bannerScore);
 			},
 			promptProfileScore: () => {
-				profileScore = prompt("Profile Score", profileScore);
+				config.profileScore = prompt("Profile Score", config.profileScore);
 			}
 		},
-		test: () => {
-			console.error(`myFollowing ${myFollowing}`);
-			console.error(`myFollowers ${myFollowers}`);
-			console.error(`passingScore ${passingScore}`);
-			console.error(`bannerScore ${bannerScore}`);
-			console.error(`profileScore ${profileScore}`);
-		}
+		test: test
 	}
 	return _this;
 })();
